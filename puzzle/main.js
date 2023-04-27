e4b4bfd0185928fbb3122f5210f1239f90593a3c
NavilP <125292694+NavilP@users.noreply.github.com>
2023-04-26 22:10:13 -0600

    Update main.js

--- puzzle/main.js
+++ puzzle/main.js
@@ -30,7 +30,7 @@ const getMatrizRandom = () => {
   let matriz = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
 
   let cajaVacia = new Caja(3, 3);
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 1000; i++) {
     const cajaVecinaRandom = cajaVacia.getCajaVecinaRandom();
     cambiaCajas(matriz, cajaVacia, cajaVecinaRandom);
     cajaVacia = cajaVecinaRandom;
